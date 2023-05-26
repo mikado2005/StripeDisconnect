57e61549e63aa289a4f594106dbea51a29cd7fb0
mikado2005 <mikado2005@gmail.com>
2023-05-26 16:41:41 -0400

    Code cleanup.

--- StripeDisconnect/ViewController.swift
+++ StripeDisconnect/ViewController.swift
@@ -169,7 +169,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate{
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = ViewController() // Your initial view controller.
+        window.rootViewController = ViewController()
         window.makeKeyAndVisible()
         self.window = window
         return true
